7978c96063300783c2c65e23d956577eddc85730
Morgan <morgan.espindola@gmail.com>
2022-11-01 20:28:23 -0300

    Get pools count on db

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,18 @@
+import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get("/pools/count", () => {
-    return { count: 10 };
+  fastify.get("/pools/count", async () => {
+    const poolsCount = await prisma.pool.count();
+    return { poolsCount };
   });
 
   await fastify.listen({ port: 3333 });
