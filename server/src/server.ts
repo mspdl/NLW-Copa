c0cd722b709a14b5b8e3d42d03d072db41aaaf72
Morgan <morgan.espindola@gmail.com>
2022-11-02 17:28:54 -0300

    Fixed server error

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     return { poolsCount };
   });
 
-  await fastify.listen({ port: 3333, host: '0.0.0.0' });
+  await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ });
 }
 
 bootstrap();
