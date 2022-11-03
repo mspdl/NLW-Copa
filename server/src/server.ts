b59d1ee6e794ea22d9094dfd1440a544bd38107a
Morgan <morgan.espindola@gmail.com>
2022-11-03 20:47:19 -0300

    Created post pools route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+import ShortUniqueId from "short-unique-id";
+import { z } from "zod";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -20,7 +22,23 @@ async function bootstrap() {
     return { poolsCount };
   });
 
-  await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ });
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+    const { title } = createPoolBody.parse(request.body);
+    const generatePoolCode = new ShortUniqueId({ length: 6 });
+    const code = String(generatePoolCode()).toUpperCase();
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    return reply.status(201).send({ code });
+  });
+
+  await fastify.listen({ port: 3333 /*host: '0.0.0.0'*/ });
 }
 
 bootstrap();
