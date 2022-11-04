c2dccd5e43d6ab79ef868e5c29b9f857e1ac29a2
Morgan <morgan.espindola@gmail.com>
2022-11-04 17:42:41 -0300

    Added Sign In with Google Button

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,18 @@
-import { Center } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+import { Center, Icon } from "native-base";
 import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Logo />
+      <Logo width={212} height={40} />
+
+      <Button
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        type="SECONDARY"
+      />
     </Center>
   );
 }
