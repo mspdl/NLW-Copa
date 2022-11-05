55bbb92bca01b3f120d1bdbb3b05630e6dc164e2
Morgan <morgan.espindola@gmail.com>
2022-11-05 14:16:16 -0300

    Added default components

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -10,11 +10,12 @@ export function Button({ title, type = "PRIMARY", ...rest }: Props) {
     <ButtonNativeBase
       w="full"
       h={14}
+      rounded="sm"
       fontSize="md"
       textTransform="uppercase"
       bg={type === "SECONDARY" ? "red.500" : "yellow.500"}
       _pressed={{
-        bg: type === "SECONDARY" ? "red.600" : "yellow.600",
+        bg: type === "SECONDARY" ? "red.400" : "yellow.600",
       }}
       _loading={{
         _spinner: { color: "black" },
