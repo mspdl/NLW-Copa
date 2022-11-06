08f77dea84233c80af59f3d03a350d23fd46b9cc
Morgan <morgan.espindola@gmail.com>
2022-11-06 14:17:17 -0300

    Add ownerId when create a pool

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -13,15 +13,32 @@ export async function poolRoutes(fastify: FastifyInstance) {
     const createPoolBody = z.object({
       title: z.string(),
     });
+
     const { title } = createPoolBody.parse(request.body);
+
     const generatePoolCode = new ShortUniqueId({ length: 6 });
     const code = String(generatePoolCode()).toUpperCase();
-    await prisma.pool.create({
-      data: {
-        title,
-        code,
-      },
-    });
+
+    let ownerId = null;
+
+    try {
+      await request.jwtVerify();
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+          ownerId: request.user.sub,
+        },
+      });
+    } catch {
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+        },
+      });
+    }
+
     return reply.status(201).send({ code });
   });
 }
