9fde84a299a350ec903765814e37be892594c944
Morgan <morgan.espindola@gmail.com>
2022-11-06 14:01:35 -0300

    Created token on user route post

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -47,8 +47,17 @@ export async function authRoutes(fastify: FastifyInstance) {
       });
     }
 
-    
+    const token = fastify.jwt.sign(
+      {
+        name: user.name,
+        avatarUrl: user.avatarUrl,
+      },
+      {
+        sub: user.id,
+        expiresIn: "7 days",
+      }
+    );
 
-    return { userInfo };
+    return { token };
   });
 }
