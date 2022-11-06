b27662305aea2e2099963d780ed279ca3e5441ea
Morgan <morgan.espindola@gmail.com>
2022-11-06 19:50:57 -0300

    Refreash pool list

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,7 +1,7 @@
 import { Octicons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { FlatList, Icon, useToast, VStack } from "native-base";
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { Button } from "../components/Button";
 import { EmptyPoolList } from "../components/EmptyPoolList";
 import { Header } from "../components/Header";
@@ -34,9 +34,11 @@ export function Pools() {
     }
   }
 
-  useEffect(() => {
-    fetchPools();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      fetchPools();
+    }, [])
+  );
 
   return (
     <VStack flex={1} bgColor="gray.900">
