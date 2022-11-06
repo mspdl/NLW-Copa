c04efd13004402683df3676209545ac0fd8f25c0
Morgan <morgan.espindola@gmail.com>
2022-11-06 19:46:33 -0300

    Added list pool from backend

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,11 +1,42 @@
 import { Octicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { Icon, VStack } from "native-base";
+import { FlatList, Icon, useToast, VStack } from "native-base";
+import { useEffect, useState } from "react";
 import { Button } from "../components/Button";
+import { EmptyPoolList } from "../components/EmptyPoolList";
 import { Header } from "../components/Header";
+import { Loading } from "../components/Loading";
+import { PoolCard, PoolCardPros } from "../components/PoolCard";
+import { api } from "../services/api";
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardPros[]>([]);
+
+  const toast = useToast();
+
   const { navigate } = useNavigation();
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/pools");
+      setPools(response.data.pools);
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        title: "Não foi possível carregar os bolões",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchPools();
+  }, []);
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -26,6 +57,20 @@ export function Pools() {
           onPress={() => navigate("findPool")}
         />
       </VStack>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={pools}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          ListEmptyComponent={() => <EmptyPoolList />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          px={5}
+        />
+      )}
     </VStack>
   );
 }
