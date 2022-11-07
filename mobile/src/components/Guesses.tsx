6f099f53394d8e9165fb9548eca8574d420cce7e
Morgan <morgan.espindola@gmail.com>
2022-11-06 22:46:06 -0300

    Register guesses

--- mobile/src/components/Guesses.tsx
+++ mobile/src/components/Guesses.tsx
@@ -2,6 +2,7 @@ import { FlatList, useToast } from "native-base";
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
 import { Game, GameProps } from "./Game";
+import { Loading } from "./Loading";
 
 interface Props {
   poolId: string;
@@ -33,9 +34,45 @@ export function Guesses({ poolId }: Props) {
     }
   }
 
+  async function handleGuessConfirm(gameId: string) {
+    try {
+      if (!firstTeamPoints.trim() || !secondTeamPoints.trim()) {
+        return toast.show({
+          title: "Informe o placar do palpite",
+          placement: "top",
+          bgColor: "red.500",
+        });
+      }
+
+      await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+        firstTeamPoints: Number(firstTeamPoints),
+        secondTeamPoints: Number(secondTeamPoints),
+      });
+
+      toast.show({
+        title: "Palpite realizado com sucesso",
+        placement: "top",
+        bgColor: "green.500",
+      });
+
+      fetchGames();
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        title: "Não foi possível enviar o palpite",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
+  }
+
   useEffect(() => {
     fetchGames();
   }, [poolId]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <FlatList
@@ -46,7 +83,9 @@ export function Guesses({ poolId }: Props) {
           data={item}
           setFirstTeamPoints={setFirstTeamPoints}
           setSecondTeamPoints={setSecondTeamPoints}
-          onGuessConfirm={() => {}}
+          onGuessConfirm={() => {
+            handleGuessConfirm(item.id);
+          }}
         />
       )}
     ></FlatList>
